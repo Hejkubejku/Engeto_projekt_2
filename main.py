a2dc0929c65ac3bfce5223ea1bd1d07f1b456b6b
Jakub Heidlberk <57895852+Hejkubejku@users.noreply.github.com>
2025-05-05 09:45:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from time import time as t
 
 def four_code_gen ()-> int:
     print(f"Hi, there!\nI've generated a random 4 digit number for you.\nLet's play a bulls and cows game.")
-    return int("".join(s.sample("123456789", 4)))
+    return int("".join(s("123456789", 4)))
 
 def choose_number () -> int:
     while True:
@@ -33,7 +33,7 @@ def compare_numbers (number: int, guess: int) -> tuple:
     return bulls, cows
 
 def main() -> str:
-    start = t.time()
+    start = t()
     number = four_code_gen()
     attempts = 0
     while True:
@@ -42,7 +42,7 @@ def main() -> str:
         bulls, cows = compare_numbers(number, guess)
         print(f"{'Bull: ' if bulls == 1 else 'Bulls: '}{bulls}, {'Cow: ' if cows == 1 else 'Cows: '} {cows}")
         if bulls == 4:
-            end = t.time()
+            end = t()
             print(f"Congratulations! You've guessed the number {number} in {attempts} attempts and {int(end - start) // 60} mins and {int(end - start) % 60} seconds.")
             break
     return "Game over!"
